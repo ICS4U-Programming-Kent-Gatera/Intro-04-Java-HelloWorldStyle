3a7bb03e61debb1d880708390d222171ebed2d11
Kent Gatera <kent.gatera@stu.ocsb.ca>
2024-02-13 16:09:22 +0000

    Style checking complete

---
+++ HelloWorldStyle.java
@@ -0,0 +1,25 @@
+/**
+ * My program Says "Hello World!".
+ *
+ * @author  Kent Gatera
+ * @version 1.0
+ * @since   2024-Feb-8
+ */
+
+public class HelloWorldStyle {
+    /**
+    * Pleases the style checker.
+    *
+    */
+    private static void helloWorld() {
+    }
+
+    /**
+    * Method Print hello World.
+    *
+    * @param args args
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
